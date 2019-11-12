c1ee890632d841b0a4b40e78bda597d4a3248b69
manojkhatokar <manojvkhatokar@gmail.com>
2019-11-12 23:26:55 +0530

    now working on ubuntu as well

--- Assets/scripts/image_upload_to_php_file.cs
+++ Assets/scripts/image_upload_to_php_file.cs
@@ -12,12 +12,16 @@ public class image_upload_to_php_file : MonoBehaviour
     public InputField port_forwarding_ip_address;
     public static string ipaddress;
 
+    public static string data = "";
+
+
+
     public void upload_data_to_php_file()
     {
 
         WWWForm form = new WWWForm();
-        byte[] byteArray = File.ReadAllBytes(Application.dataPath + "/captured_images/per_session_images/CameraScreenshot" + screenshot_taker.image_index + ".png");
-        form.AddBinaryData("image_blob_data", byteArray, Application.dataPath + "/captured_images/per_session_images/CameraScreenshot" + screenshot_taker.image_index + ".png");
+        //byte[] byteArray = File.ReadAllBytes(Application.dataPath + "/captured_images/per_session_images/CameraScreenshot" + screenshot_taker.image_index + ".png");
+        //form.AddBinaryData("image_blob_data", byteArray, Application.dataPath + "/captured_images/per_session_images/CameraScreenshot" + screenshot_taker.image_index + ".png");
 
         StartCoroutine(upload_data_to_php_file_ienumerator());
         
@@ -37,8 +41,16 @@ public class image_upload_to_php_file : MonoBehaviour
             Debug.Log(" using firebase database now");
 
         }
+
+        else if(port_forwarding_ip_address.text == "localhost")
+        {
+            ipaddress = "http://localhost/object_detection_connection.php";
+
+        }
         else
-        ipaddress = "http://"  + port_forwarding_ip_address.text + "/object_detection_connection.php";
+        {
+            ipaddress = "http://" + port_forwarding_ip_address.text + "/object_detection_connection.php";
+        }
         WWW www = new WWW(ipaddress, form);
         yield return www;
 
@@ -64,7 +76,7 @@ public class image_upload_to_php_file : MonoBehaviour
         //    {
         //        Debug.Log(www2.downloadHandler.text);
 
-               
+
 
         //    }
 
@@ -72,6 +84,65 @@ public class image_upload_to_php_file : MonoBehaviour
 
         //}
 
+
+
+
+        // Encode texture into PNG
+        //byte[] bytes = screenshot_taker.renderResult.EncodeToJPG();
+        // Create a Web Form
+        //WWWForm form = new WWWForm();
+        //form.AddField("frameCount", Time.frameCount.ToString());
+        //form.AddBinaryData("image_blob_data", bytes, "captured_image.jpg", "image/jpg");
+        // Upload to a cgi script
+        //using (var w = UnityWebRequest.Post("/opt/lampp/cgi-bin/test-cgi", form))
+        //{
+        //    w.chunkedTransfer = false;
+        //    yield return w.SendWebRequest();
+        //    if (w.error != null)
+        //    {
+        //        print("error" + w.error);
+        //    }
+        //    else
+        //    {
+        //        print("Finished Uploading Captured Image: " + w.responseCode);
+        //    }
+        //}
+
+
+
+        //   data="hi";
+        //JsonUtility.ToJson(data);
+        //Debug.Log(data);
+
+        //using (UnityWebRequest www_json = UnityWebRequest.Put("0.0.0.0:5000/predict", data))
+
+        //{
+        //    www_json.SetRequestHeader("Content-Type", "application/json");
+        //    //Debug.Log("");
+
+        //}   
+
+
+        //######################################trying this out still for json direct send to flask_app.py
+        string json = "hi";
+        var jsonBinary = System.Text.Encoding.UTF8.GetBytes(json);
+
+        DownloadHandlerBuffer downloadHandlerBuffer = new DownloadHandlerBuffer();
+
+        UploadHandlerRaw uploadHandlerRaw = new UploadHandlerRaw(jsonBinary);
+        uploadHandlerRaw.contentType = "application/json";
+
+        UnityWebRequest www_json =
+            new UnityWebRequest("0.0.0.0:5000/predict", "POST", downloadHandlerBuffer, uploadHandlerRaw);
+
+        yield return www_json.SendWebRequest();
+
+        if (www_json.isNetworkError)
+            Debug.LogError(string.Format("{0}: {1}", www_json.url, www_json.error));
+        else
+            Debug.Log(string.Format("Response: {0}", www_json.downloadHandler.text));
+    
+    
     }
 
     
--- Assets/scripts/screenshot_taker.cs
+++ Assets/scripts/screenshot_taker.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class screenshot_taker : MonoBehaviour
 {
@@ -8,6 +16,11 @@ public class screenshot_taker : MonoBehaviour
      private static screenshot_taker instance;
     private  Camera myCamera;
     private bool takeScreenshotOnNextFrame;
+
+    public static byte[] byteArray;//this is the variable that contains the screenshot in the form of byte 
+    public static Texture2D renderResult;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +41,15 @@ public class screenshot_taker : MonoBehaviour
             takeScreenshotOnNextFrame = false;
             RenderTexture renderTexture = myCamera.targetTexture;
 
-            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+
             Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
             renderResult.ReadPixels(rect, 0, 0);
-            byte[] byteArray = renderResult.EncodeToJPG();
+            byteArray = renderResult.EncodeToJPG();
+
             System.IO.File.WriteAllBytes(Application.dataPath + "/captured_images/per_session_images/CameraScreenshot"+image_index+".png", byteArray);
             Debug.Log("saved CameraScreenshot.png");
+            Debug.Log(Application.dataPath + "/captured_images/per_session_images/CameraScreenshot" + image_index + ".png");
             
 
             RenderTexture.ReleaseTemporary(renderTexture);
